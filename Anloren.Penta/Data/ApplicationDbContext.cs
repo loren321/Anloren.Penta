1030e5c7f1a65c924c8e4325426c2fe324ef6fb1
Antonio Smiljanić <antonio@umbeon.com>
2023-01-23 01:03:49 +0200

    Initial commit

---
+++ Anloren.Penta/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Anloren.Penta.Models;
+
+using Duende.IdentityServer.EntityFramework.Options;
+
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace Anloren.Penta.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions options, IOptions<OperationalStoreOptions> operationalStoreOptions)
+            : base(options, operationalStoreOptions)
+        {
+
+        }
+    }
+}
---
+++ Anloren.Penta/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Anloren.Penta.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+
+    }
+}
